b714ddf813aa6a9f4ab9415f4e3b963e7c8a7f3b
Abdallah Safar <abdallah.safar01@gmail.com>
2021-04-28 11:46:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function map(arr,func) {
 }
 
 
+<<<<<<< HEAD
 // function reduce(arr,starting) {
 //   return arr.reduce((memo,i) => {
 //     if(starting === undefined) {
@@ -20,6 +21,13 @@ function reduce(arr,func) {
   return arr.reduce(func);
 }
 
+=======
+function reduce(arr,func,starting) {
+  return arr.reduce(func,starting);
+}
+
+
+>>>>>>> 9aed1101c83c864d6ee3f5732903c80568b5ff54
 console.log(map([1, 2, 3, -9], function(a){ return a * a }))
 // console.log(reduce([1,2,3],(memo,i) => {
 //   memo *= i;
